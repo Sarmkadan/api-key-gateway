59e850b7319613b0be996d08bd3bffc0bc092877
Vladyslav Zaiets <rutova2@gmail.com>
2021-01-06 17:05:20 +0200

    feat: small CorrelationContextMiddleware helpers

---
+++ src/ApiKeyGateway/Middleware/CorrelationContextMiddlewareExtensions.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+namespace ApiKeyGateway.Middleware;
+
+/// <summary>
+/// Extension methods for <see cref="CorrelationContextMiddleware"/> that provide convenient access to correlation context values
+/// and middleware configuration helpers.
+/// </summary>
+public static class CorrelationContextMiddlewareExtensions
+{
+    /// <summary>
+    /// Gets the correlation ID from the HTTP context items set by the correlation context middleware.
+    /// </summary>
+    /// <param name="context">The HTTP context.</param>
+    /// <returns>The correlation ID, or null if not set.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="context"/> is null.</exception>
+    public static string? GetCorrelationId(this HttpContext context)
+    {
+        ArgumentNullException.ThrowIfNull(context);
+
+        return context.Items.TryGetValue("CorrelationId", out var value)
+            ? value as string
+            : null;
+    }
+
+    /// <summary>
+    /// Gets the API key ID from the HTTP context items set by the correlation context middleware.
+    /// </summary>
+    /// <param name="context">The HTTP context.</param>
+    /// <returns>The API key ID, or null if not set.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="context"/> is null.</exception>
+    public static string? GetApiKeyId(this HttpContext context)
+    {
+        ArgumentNullException.ThrowIfNull(context);
+
+        return context.Items.TryGetValue("ApiKeyId", out var value)
+            ? value as string
+            : null;
+    }
+
+    /// <summary>
+    /// Gets the client IP address from the HTTP context items set by the correlation context middleware.
+    /// </summary>
+    /// <param name="context">The HTTP context.</param>
+    /// <returns>The client IP address, or null if not set.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="context"/> is null.</exception>
+    public static string? GetClientIp(this HttpContext context)
+    {
+        ArgumentNullException.ThrowIfNull(context);
+
+        return context.Items.TryGetValue("ClientIp", out var value)
+            ? value as string
+            : null;
+    }
+
+    /// <summary>
+    /// Extracts all correlation context values as a dictionary for logging or diagnostics purposes.
+    /// </summary>
+    /// <param name="context">The HTTP context.</param>
+    /// <returns>A read-only dictionary containing all correlation context values.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="context"/> is null.</exception>
+    public static IReadOnlyDictionary<string, object?> GetCorrelationContext(this HttpContext context)
+    {
+        ArgumentNullException.ThrowIfNull(context);
+
+        var result = new Dictionary<string, object?>(StringComparer.Ordinal);
+
+        if (context.Items.TryGetValue("CorrelationId", out var correlationId))
+        {
+            result["CorrelationId"] = correlationId;
+        }
+
+        if (context.Items.TryGetValue("ApiKeyId", out var apiKeyId))
+        {
+            result["ApiKeyId"] = apiKeyId;
+        }
+
+        if (context.Items.TryGetValue("ClientIp", out var clientIp))
+        {
+            result["ClientIp"] = clientIp;
+        }
+
+        return result.AsReadOnly();
+    }
+
+    /// <summary>
+    /// Determines whether the correlation context has been initialized in the HTTP context.
+    /// </summary>
+    /// <param name="context">The HTTP context.</param>
+    /// <returns>True if correlation context values are present; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="context"/> is null.</exception>
+    public static bool HasCorrelationContext(this HttpContext context)
+    {
+        ArgumentNullException.ThrowIfNull(context);
+
+        return context.Items.ContainsKey("CorrelationId");
+    }
+}
