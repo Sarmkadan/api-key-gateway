4969abcabc3777ec9bd74a06c5c0b31fe97d1dfd
Vladyslav Zaiets <rutova2@gmail.com>
2021-01-25 19:05:51 +0200

    feat: CollectionExtensionsJson (de)serialization helpers

---
+++ src/ApiKeyGateway/Extensions/CollectionExtensionsJsonExtensions.cs
@@ -0,0 +1,87 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =====================================================================
+
+using System.Text.Json;
+
+namespace ApiKeyGateway.Extensions;
+
+/// <summary>
+/// Provides JSON serialization and deserialization extensions for collection operations.
+/// These extensions work with types that utilize <see cref="CollectionExtensions"/> methods.
+/// </summary>
+public static class CollectionExtensionsJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+    };
+
+    /// <summary>
+    /// Serializes a collection to a JSON string.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the collection.</typeparam>
+    /// <param name="source">The collection to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the collection.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> is <see langword="null"/>.</exception>
+    public static string ToJson<T>(this IEnumerable<T> source, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(source);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonSerializerOptions)
+            {
+                WriteIndented = true,
+            }
+            : _jsonSerializerOptions;
+
+        return JsonSerializer.Serialize(source, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a collection.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the collection.</typeparam>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>A collection instance, or <see langword="null"/> if the JSON is empty or whitespace.</returns>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static IEnumerable<T>? FromJson<T>(string json)
+    {
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<IEnumerable<T>>(json, _jsonSerializerOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a collection.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the collection.</typeparam>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized collection if successful.</param>
+    /// <returns><see langword="true"/> if deserialization succeeds; otherwise, <see langword="false"/>.</returns>
+    public static bool TryFromJson<T>(string json, out IEnumerable<T>? value)
+    {
+        value = null;
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return true;
+        }
+
+        try
+        {
+            value = JsonSerializer.Deserialize<IEnumerable<T>>(json, _jsonSerializerOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+}
