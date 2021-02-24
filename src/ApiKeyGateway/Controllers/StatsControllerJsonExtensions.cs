77a9bb244599453819e855140c6c541817320c64
Vladyslav Zaiets <rutova2@gmail.com>
2021-02-24 08:29:05 +0200

    serialize StatsControllerJson properly

---
+++ src/ApiKeyGateway/Controllers/StatsControllerJsonExtensions.cs
@@ -0,0 +1,81 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+//
+// System.Text.Json serialization helpers for StatsController
+// =============================================================================
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ApiKeyGateway.Controllers;
+
+/// <summary>
+/// Provides System.Text.Json serialization extensions for <see cref="StatsController"/>.
+/// </summary>
+public static class StatsControllerJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+    };
+
+    /// <summary>
+    /// Serializes the <see cref="StatsController"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The controller instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the controller.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this StatsController value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonSerializerOptions)
+            {
+                WriteIndented = true
+            }
+            : _jsonSerializerOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="StatsController"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>A <see cref="StatsController"/> instance, or null if the JSON is invalid.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    public static StatsController? FromJson(string json)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        return JsonSerializer.Deserialize<StatsController>(json, _jsonSerializerOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="StatsController"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized controller instance if successful.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    public static bool TryFromJson(string json, out StatsController? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<StatsController>(json, _jsonSerializerOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
