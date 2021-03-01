8509b7e7cd18a2dd27e32ca1f64e84160632d2d3
Vladyslav Zaiets <rutova2@gmail.com>
2021-03-01 17:33:31 +0200

    add UsageTrackingServiceTetssJson json helpers

---
+++ tests/api-key-gateway.Tests/UsageTrackingServiceTestsJsonExtensions.cs
@@ -0,0 +1,69 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ApiKeyGateway.Tests;
+
+/// <summary>
+/// Provides JSON serialization helpers for <see cref="UsageTrackingServiceTests"/>.
+/// </summary>
+public static class UsageTrackingServiceTestsJsonExtensions
+{
+    /// <summary>
+    /// JSON serialization options configured for camelCase property naming.
+    /// </summary>
+    private static readonly JsonSerializerOptions Options = new()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false
+    };
+
+    /// <summary>
+    /// Serializes the <see cref="UsageTrackingServiceTests"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The test instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation.</param>
+    /// <returns>A JSON string representation of the test instance.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is null.</exception>
+    public static string ToJson(this UsageTrackingServiceTests value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        Options.WriteIndented = indented;
+        return JsonSerializer.Serialize(value, Options);
+    }
+
+    /// <summary>
+    /// Deserializes a <see cref="UsageTrackingServiceTests"/> instance from a JSON string.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>A deserialized <see cref="UsageTrackingServiceTests"/> instance.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="json"/> is null or empty.</exception>
+    /// <exception cref="JsonException">Thrown if JSON deserialization fails.</exception>
+    public static UsageTrackingServiceTests FromJson(string json)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+        return JsonSerializer.Deserialize<UsageTrackingServiceTests>(json, Options)
+            ?? throw new JsonException("Failed to deserialize UsageTrackingServiceTests.");
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a <see cref="UsageTrackingServiceTests"/> instance from a JSON string.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">The deserialized test instance, or null if deserialization failed.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    public static bool TryFromJson(string json, out UsageTrackingServiceTests? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+        try
+        {
+            value = JsonSerializer.Deserialize<UsageTrackingServiceTests>(json, Options);
+            return value is not null;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
