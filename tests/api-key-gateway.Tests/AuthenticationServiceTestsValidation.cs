944f964887d80b7d3c589b6493d4c47199063e19
Vladyslav Zaiets <rutova2@gmail.com>
2021-03-31 15:14:32 +0300

    Guard clauses in AuthenticationService

---
+++ tests/api-key-gateway.Tests/AuthenticationServiceTestsValidation.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Xunit;
+
+namespace ApiKeyGateway.Tests
+{
+    /// <summary>
+    /// Provides validation helpers for <see cref="AuthenticationServiceTests"/> test class.
+    /// </summary>
+    public static class AuthenticationServiceTestsValidation
+    {
+        /// <summary>
+        /// Validates the test class instance and returns a list of validation problems.
+        /// </summary>
+        /// <param name="value">The test class instance to validate.</param>
+        /// <returns>Collection of human-readable validation problems.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+        public static IReadOnlyList<string> Validate(this AuthenticationServiceTests value)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+
+            var problems = new List<string>();
+
+            // Validate test method attributes and naming conventions
+            var testMethods = value.GetType()
+                .GetMethods(BindingFlags.Public | BindingFlags.Instance)
+                .Where(m => m.Name.StartsWith("AuthenticateAsync") ||
+                            m.Name.StartsWith("ValidateIpAsync") ||
+                            m.Name.StartsWith("LogAuthenticationAttemptAsync"));
+
+            foreach (var method in testMethods)
+            {
+                // Check for null/empty test names
+                if (string.IsNullOrEmpty(method.Name))
+                {
+                    problems.Add($"Test method has null/empty name: {method.DeclaringType?.Name}");
+                }
+
+                // Check for test method attributes
+                if (!method.GetCustomAttributes(typeof(FactAttribute), false).Any() &&
+                    !method.GetCustomAttributes(typeof(TheoryAttribute), false).Any())
+                {
+                    problems.Add($"Test method {method.Name} is missing [Fact] or [Theory] attribute");
+                }
+            }
+
+            return problems;
+        }
+
+        /// <summary>
+        /// Checks if the test class instance is valid according to validation rules.
+        /// </summary>
+        /// <param name="value">The test class instance to validate.</param>
+        /// <returns>True if valid, false otherwise.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+        public static bool IsValid(this AuthenticationServiceTests value)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+            return !value.Validate().Any();
+        }
+
+        /// <summary>
+        /// Validates the test class instance and throws if invalid.
+        /// </summary>
+        /// <param name="value">The test class instance to validate.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when validation fails with list of problems.</exception>
+        public static void EnsureValid(this AuthenticationServiceTests value)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+
+            var problems = value.Validate();
+            if (problems.Any())
+            {
+                throw new ArgumentException(string.Join("\n", problems), nameof(value));
+            }
+        }
+    }
+}
