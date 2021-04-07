39889120eb3c1e28227cfca69ec4bef202853fa0
Vladyslav Zaiets <rutova2@gmail.com>
2021-04-07 19:41:28 +0300

    refactor: reshuffle AuditLogServiceTests a bit.

--- tests/api-key-gateway.Tests/AuditLogServiceTests.cs
+++ tests/api-key-gateway.Tests/AuditLogServiceTests.cs
@@ -14,12 +14,21 @@ using Moq;
 
 namespace ApiKeyGateway.Tests;
 
+/// <summary>
+/// Contains unit tests for the <see cref="AuditLogService"/> class.
+/// Tests verify that the audit logging functionality works correctly,
+/// including constructor validation, logging operations, and log retrieval methods.
+/// </summary>
 public class AuditLogServiceTests
 {
     private readonly Mock<IAuditLogRepository> _repositoryMock;
     private readonly Mock<ILogger<AuditLogService>> _loggerMock;
     private readonly AuditLogService _sut;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AuditLogServiceTests"/> class.
+    /// Sets up mock repositories and logger for testing audit log service functionality.
+    /// </summary>
     public AuditLogServiceTests()
     {
         _repositoryMock = new Mock<IAuditLogRepository>();
@@ -27,6 +36,10 @@ public class AuditLogServiceTests
         _sut = new AuditLogService(_repositoryMock.Object, _loggerMock.Object);
     }
 
+    /// <summary>
+    /// Tests that the constructor throws an <see cref="ArgumentNullException"/> when a null repository is provided.
+    /// Ensures proper parameter validation for dependency injection.
+    /// </summary>
     [Fact]
     public void Constructor_NullRepository_ThrowsArgumentNullException()
     {
@@ -34,6 +47,10 @@ public class AuditLogServiceTests
         act.Should().Throw<ArgumentNullException>().WithParameterName("repository");
     }
 
+    /// <summary>
+    /// Tests that the constructor throws an <see cref="ArgumentNullException"/> when a null logger is provided.
+    /// Ensures proper parameter validation for dependency injection.
+    /// </summary>
     [Fact]
     public void Constructor_NullLogger_ThrowsArgumentNullException()
     {
@@ -41,6 +58,10 @@ public class AuditLogServiceTests
         act.Should().Throw<ArgumentNullException>().WithParameterName("logger");
     }
 
+    /// <summary>
+    /// Tests that <see cref="AuditLogService.LogAsync(AuditLog)"/> throws an <see cref="ArgumentNullException"/> when a null log is provided.
+    /// Ensures proper parameter validation for audit log creation.
+    /// </summary>
     [Fact]
     public async Task LogAsync_NullLog_ThrowsArgumentNullException()
     {
@@ -48,6 +69,10 @@ public class AuditLogServiceTests
         await act.Should().ThrowAsync<ArgumentNullException>().WithParameterName("log");
     }
 
+    /// <summary>
+    /// Tests that <see cref="AuditLogService.LogAsync(AuditLog)"/> successfully creates a log entry in the repository when provided with valid data.
+    /// Verifies that the service properly delegates to the repository and passes the correct log object.
+    /// </summary>
     [Fact]
     public async Task LogAsync_ValidLog_CreatesInRepository()
     {
@@ -70,6 +95,10 @@ public class AuditLogServiceTests
         _repositoryMock.Verify(r => r.CreateAsync(log), Times.Once);
     }
 
+    /// <summary>
+    /// Tests that <see cref="AuditLogService.LogAsync(AuditLog)"/> handles repository exceptions gracefully without throwing.
+    /// Verifies that the service catches and suppresses repository errors to maintain system stability.
+    /// </summary>
     [Fact]
     public async Task LogAsync_RepositoryThrows_FailsSilently()
     {
@@ -90,6 +119,10 @@ public class AuditLogServiceTests
         await act.Should().NotThrowAsync("service should handle repository errors gracefully");
     }
 
+    /// <summary>
+    /// Tests that <see cref="AuditLogService.LogAsync(AuditLog)"/> logs an information message when a log entry is successfully created.
+    /// Verifies that the service properly logs the audit action for monitoring and debugging purposes.
+    /// </summary>
     [Fact]
     public async Task LogAsync_SuccessfulLog_LogsInformationMessage()
     {
@@ -132,6 +165,10 @@ public class AuditLogServiceTests
         _repositoryMock.Verify(r => r.GetByResourceIdAsync(It.IsAny<string>(), It.IsAny<int>()), Times.Never);
     }
 
+    /// <summary>
+    /// Tests that <see cref="AuditLogService.GetLogsAsync(string,int)"/> returns audit logs from the repository when provided with a valid resource ID.
+    /// Verifies that the service properly delegates to the repository and returns the expected log entries.
+    /// </summary>
     [Fact]
     public async Task GetLogsAsync_ValidResourceId_ReturnsLogsFromRepository()
     {
@@ -152,6 +189,10 @@ public class AuditLogServiceTests
         result.Should().BeEquivalentTo(logs);
     }
 
+    /// <summary>
+    /// Tests that <see cref="AuditLogService.GetLogsAsync(string,int)"/> properly passes a custom limit to the repository.
+    /// Verifies that the service correctly handles pagination and respects the specified limit parameter.
+    /// </summary>
     [Fact]
     public async Task GetLogsAsync_WithCustomLimit_PassesLimitToRepository()
     {
@@ -167,6 +208,10 @@ public class AuditLogServiceTests
         _repositoryMock.Verify(r => r.GetByResourceIdAsync("key-999", 50), Times.Once);
     }
 
+    /// <summary>
+    /// Tests that <see cref="AuditLogService.GetLogsAsync(string,int)"/> returns an empty list when no logs are found for the specified resource ID.
+    /// Verifies that the service properly handles the case where no audit logs exist for a given resource.
+    /// </summary>
     [Fact]
     public async Task GetLogsAsync_NoLogsFound_ReturnsEmptyList()
     {
@@ -179,6 +224,10 @@ public class AuditLogServiceTests
         result.Should().BeEmpty();
     }
 
+    /// <summary>
+    /// Tests that <see cref="AuditLogService.GetLogsForPeriodAsync(DateTime,DateTime)"/> throws an <see cref="ArgumentException"/> when the end date is before the start date.
+    /// Verifies that the service properly validates date ranges to prevent invalid queries.
+    /// </summary>
     [Fact]
     public async Task GetLogsForPeriodAsync_EndDateBeforeStartDate_ThrowsArgumentException()
     {
@@ -190,6 +239,10 @@ public class AuditLogServiceTests
         await act.Should().ThrowAsync<ArgumentException>().WithMessage("*End date must be after start date*");
     }
 
+    /// <summary>
+    /// Tests that <see cref="AuditLogService.GetLogsForPeriodAsync(DateTime,DateTime)"/> returns audit logs from the repository for a valid date range.
+    /// Verifies that the service properly queries the repository with the correct date parameters and returns matching logs.
+    /// </summary>
     [Fact]
     public async Task GetLogsForPeriodAsync_ValidDateRange_ReturnsLogsFromRepository()
     {
@@ -212,6 +265,10 @@ public class AuditLogServiceTests
         result.Should().BeEquivalentTo(logs);
     }
 
+    /// <summary>
+    /// Tests that <see cref="AuditLogService.GetLogsForPeriodAsync(DateTime,DateTime)"/> returns an empty list when no logs exist within the specified date range.
+    /// Verifies that the service properly handles the case where no audit logs fall within the requested time period.
+    /// </summary>
     [Fact]
     public async Task GetLogsForPeriodAsync_NoLogsInRange_ReturnsEmptyList()
     {
@@ -227,6 +284,11 @@ public class AuditLogServiceTests
         result.Should().BeEmpty();
     }
 
+    /// <summary>
+    /// Tests that <see cref="AuditLogService.CleanupOldLogsAsync(int)"/> throws an <see cref="ArgumentException"/> when provided with invalid (non-positive) retention days.
+    /// Verifies that the service properly validates the retention period parameter.
+    /// </summary>
+    /// <param name="retentionDays">The invalid retention days value to test (0, negative).</param>
     [Theory]
     [InlineData(0)]
     [InlineData(-1)]
@@ -237,6 +299,10 @@ public class AuditLogServiceTests
         await act.Should().ThrowAsync<ArgumentException>().WithMessage("*Retention days must be positive*");
     }
 
+    /// <summary>
+    /// Tests that <see cref="AuditLogService.CleanupOldLogsAsync(int)"/> successfully deletes old logs and logs the operation as successful when provided with valid retention days.
+    /// Verifies that the service properly calculates the cutoff date and delegates the cleanup operation to the repository.
+    /// </summary>
     [Fact]
     public async Task CleanupOldLogsAsync_ValidRetentionDays_DeletesOldLogsAndLogsSuccess()
     {
@@ -251,6 +317,10 @@ public class AuditLogServiceTests
         )), Times.Once);
     }
 
+    /// <summary>
+    /// Tests that <see cref="AuditLogService.CleanupOldLogsAsync(int)"/> properly logs a zero count message when no logs are deleted during cleanup.
+    /// Verifies that the service correctly handles the case where the cleanup operation doesn't remove any records.
+    /// </summary>
     [Fact]
     public async Task CleanupOldLogsAsync_NoLogsDeleted_LogsZeroCount()
     {
@@ -263,6 +333,10 @@ public class AuditLogServiceTests
         _repositoryMock.Verify(r => r.DeleteOlderThanAsync(It.IsAny<DateTime>()), Times.Once);
     }
 
+    /// <summary>
+    /// Tests that <see cref="AuditLogService.CleanupOldLogsAsync(int)"/> handles repository exceptions gracefully during cleanup without throwing.
+    /// Verifies that the service catches and suppresses repository errors to maintain system stability during cleanup operations.
+    /// </summary>
     [Fact]
     public async Task CleanupOldLogsAsync_RepositoryThrows_FailsSilently()
     {
@@ -285,6 +359,10 @@ public class AuditLogServiceTests
         );
     }
 
+    /// <summary>
+    /// Tests that <see cref="AuditLogService.LogAsync(AuditLog)"/> handles concurrent logging operations successfully.
+    /// Verifies that the service can process multiple simultaneous log requests without errors or race conditions.
+    /// </summary>
     [Fact]
     public async Task LogAsync_ConcurrentLogging_AllSucceed()
     {
@@ -308,6 +386,10 @@ public class AuditLogServiceTests
         _repositoryMock.Verify(r => r.CreateAsync(It.IsAny<AuditLog>()), Times.Exactly(50));
     }
 
+    /// <summary>
+    /// Tests that <see cref="AuditLogService.GetLogsAsync(string,int)"/> uses a default limit of 100 when no limit is specified.
+    /// Verifies that the service provides a sensible default pagination value for retrieving audit logs.
+    /// </summary>
     [Fact]
     public async Task GetLogsAsync_DefaultLimit_Uses100()
     {
