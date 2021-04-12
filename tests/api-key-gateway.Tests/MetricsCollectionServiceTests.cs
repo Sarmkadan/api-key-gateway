5cf091654c00dbdc7a96efc0cf6749c6ccb7ca49
Vladyslav Zaiets <rutova2@gmail.com>
2021-04-12 10:15:43 +0300

    polish MetricsCollectionServiceTests

--- tests/api-key-gateway.Tests/MetricsCollectionServiceTests.cs
+++ tests/api-key-gateway.Tests/MetricsCollectionServiceTests.cs
@@ -6,17 +6,29 @@ using Moq;
 
 namespace ApiKeyGateway.Tests;
 
+/// <summary>
+/// Contains unit tests for <see cref="MetricsCollectionService"/>.
+/// Verifies that request and error recording correctly updates the service's metrics snapshot.
+/// </summary>
 public class MetricsCollectionServiceTests
 {
     private readonly Mock<ILogger<MetricsCollectionService>> _loggerMock;
     private readonly MetricsCollectionService _sut;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MetricsCollectionServiceTests"/> class.
+    /// Sets up a mocked <see cref="ILogger{MetricsCollectionService}"/> and creates the service under test.
+    /// </summary>
     public MetricsCollectionServiceTests()
     {
         _loggerMock = new Mock<ILogger<MetricsCollectionService>>();
         _sut = new MetricsCollectionService(_loggerMock.Object);
     }
 
+    /// <summary>
+    /// Verifies that calling <see cref="MetricsCollectionService.RecordRequest"/>
+    /// with valid data increments the total request count and updates the average latency.
+    /// </summary>
     [Fact]
     public void RecordRequest_ValidData_UpdatesMetrics()
     {
@@ -29,6 +41,10 @@ public class MetricsCollectionServiceTests
         snapshot.AverageLatencyMs.Should().Be(100);
     }
 
+    /// <summary>
+    /// Verifies that calling <see cref="MetricsCollectionService.RecordError"/>
+    /// with valid data increments the total error count and records the error code.
+    /// </summary>
     [Fact]
     public void RecordError_ValidData_UpdatesMetrics()
     {
