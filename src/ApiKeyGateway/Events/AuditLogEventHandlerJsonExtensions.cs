33a89e4b69f6a0d4eb50e9023dd152e21f7f1a2d
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 13:33:56 +0300

    small refactor - AuditLogEventHandlerJsonExtensions.

--- src/ApiKeyGateway/Events/AuditLogEventHandlerJsonExtensions.cs
+++ src/ApiKeyGateway/Events/AuditLogEventHandlerJsonExtensions.cs
@@ -6,12 +6,12 @@ using System.Text.Json;
 using ApiKeyGateway.Events;
 
 /// <summary>
-/// JSON (de)serialization helpers for <see cref="AuditLogEventHandler"/>.
+/// Provides JSON (de)serialization extensions for <see cref="AuditLogEventHandler"/>.
 /// </summary>
 public static class AuditLogEventHandlerJsonExtensions
 {
     /// <summary>
-    /// Cached serializer options that use camel‑case property names.
+    /// Cached serializer options configured for camelCase property names and web defaults.
     /// </summary>
     private static readonly JsonSerializerOptions _options = new(JsonSerializerDefaults.Web)
     {
@@ -22,27 +22,21 @@ public static class AuditLogEventHandlerJsonExtensions
     /// Serializes the <see cref="AuditLogEventHandler"/> instance to a JSON string.
     /// </summary>
     /// <param name="value">The handler instance to serialize.</param>
-    /// <param name="indented">If <c>true</c>, the output JSON will be formatted with indentation.</param>
+    /// <param name="indented">If <see langword="true"/>, the output JSON will be formatted with indentation.</param>
     /// <returns>A JSON representation of <paramref name="value"/>.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <c>null</c>.</exception>
-    public static string ToJson(this AuditLogEventHandler value, bool indented = false)
-    {
-        ArgumentNullException.ThrowIfNull(value);
-
-        var options = indented
-            ? new JsonSerializerOptions(_options) { WriteIndented = true }
-            : _options;
-
-        return JsonSerializer.Serialize(value, options);
-    }
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
+    public static string ToJson(this AuditLogEventHandler value, bool indented = false) =>
+        JsonSerializer.Serialize(value, indented ? new JsonSerializerOptions(_options) { WriteIndented = true } : _options);
 
     /// <summary>
     /// Deserializes a JSON string into an <see cref="AuditLogEventHandler"/> instance.
     /// </summary>
     /// <param name="json">The JSON representation of an <see cref="AuditLogEventHandler"/>.</param>
-    /// <returns>The deserialized <see cref="AuditLogEventHandler"/>, or <c>null</c> if the JSON is <c>null</c> or empty.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <c>null</c>.</exception>
-    /// <exception cref="JsonException">Thrown when the JSON cannot be deserialized into an <see cref="AuditLogEventHandler"/>.</exception>
+    /// <returns>
+    /// The deserialized <see cref="AuditLogEventHandler"/>, or <see langword="null"/> if the JSON is <see langword="null"/> or empty.
+    /// </returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
+    /// <exception cref="JsonException">The JSON cannot be deserialized into an <see cref="AuditLogEventHandler"/>.</exception>
     public static AuditLogEventHandler? FromJson(string json)
     {
         ArgumentNullException.ThrowIfNull(json);
@@ -55,13 +49,14 @@ public static class AuditLogEventHandlerJsonExtensions
     /// <param name="json">The JSON representation of an <see cref="AuditLogEventHandler"/>.</param>
     /// <param name="value">
     /// When this method returns, contains the deserialized <see cref="AuditLogEventHandler"/> if the operation succeeded,
-    /// or <c>null</c> otherwise.
+    /// or <see langword="null"/> otherwise.
     /// </param>
-    /// <returns><c>true</c> if deserialization succeeded; otherwise, <c>false</c>.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <c>null</c>.</exception>
+    /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
     public static bool TryFromJson(string json, out AuditLogEventHandler? value)
     {
         ArgumentNullException.ThrowIfNull(json);
+
         try
         {
             value = JsonSerializer.Deserialize<AuditLogEventHandler>(json, _options);
