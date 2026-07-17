e43a006666cdbad4cd510f38c45bdf10fdd4f2e5
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    Small refactor - IntegrationTestsExtensions
    should be backwards compatible.

--- tests/api-key-gateway.Tests/IntegrationTestsExtensions.cs
+++ tests/api-key-gateway.Tests/IntegrationTestsExtensions.cs
@@ -13,78 +13,98 @@ namespace ApiKeyGateway.Tests;
 /// </summary>
 public static class IntegrationTestsExtensions
 {
-    /// <summary>
-    /// Creates a sample <see cref="UsageRecord"/> for testing purposes.
-    /// </summary>
-    /// <param name="tests">The <see cref="IntegrationTests"/> instance.</param>
-    /// <param name="apiKeyId">The API key ID to associate with the usage record.</param>
-    /// <returns>A new <see cref="UsageRecord"/> instance.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="tests"/> is null.</exception>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="apiKeyId"/> is null or empty.</exception>
-    public static UsageRecord CreateSampleUsageRecord(this IntegrationTests tests, string apiKeyId)
-    {
-        ArgumentNullException.ThrowIfNull(tests);
-        ArgumentException.ThrowIfNullOrEmpty(apiKeyId);
+	/// <summary>
+	/// Creates a sample <see cref="UsageRecord"/> for testing purposes.
+	/// </summary>
+	/// <param name="tests">The <see cref="IntegrationTests"/> instance.</param>
+	/// <param name="apiKeyId">The API key ID to associate with the usage record.</param>
+	/// <param name="consumerId">The consumer ID associated with the usage record. Defaults to "test-consumer".</param>
+	/// <returns>A new <see cref="UsageRecord"/> instance.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="tests"/> is null.</exception>
+	/// <exception cref="ArgumentException">Thrown when <paramref name="apiKeyId"/> or <paramref name="consumerId"/> is null or empty.</exception>
+	public static UsageRecord CreateSampleUsageRecord(this IntegrationTests tests, string apiKeyId, string consumerId = "test-consumer")
+	{
+		ArgumentNullException.ThrowIfNull(tests);
+		ArgumentException.ThrowIfNullOrEmpty(apiKeyId);
+		ArgumentException.ThrowIfNullOrEmpty(consumerId);
 
-        return new UsageRecord
-        {
-            Id = Guid.NewGuid().ToString(),
-            ApiKeyId = apiKeyId,
-            Endpoint = "/api/test",
-            Method = "GET",
-            ResponseStatusCode = 200,
-            ResponseTimeMs = 50,
-            BytesTransferred = 1024
-        };
-    }
+		return new UsageRecord
+		{
+			Id = Guid.NewGuid().ToString(),
+			ApiKeyId = apiKeyId,
+			ConsumerId = consumerId,
+			Endpoint = "/api/test",
+			Method = "GET",
+			ResponseStatusCode = 200,
+			ResponseTimeMs = 50,
+			RequestBytes = 512,
+			ResponseBytes = 1024,
+			Tags = []
+		};
+	}
 
-    /// <summary>
-    /// Creates a sample <see cref="AuditLog"/> for testing purposes.
-    /// </summary>
-    /// <param name="tests">The <see cref="IntegrationTests"/> instance.</param>
-    /// <param name="resourceId">The ID of the resource associated with the log.</param>
-    /// <param name="resourceType">The type of the resource.</param>
-    /// <param name="action">The <see cref="AuditAction"/> performed.</param>
-    /// <returns>A new <see cref="AuditLog"/> instance.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="tests"/> is null.</exception>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="resourceId"/> or <paramref name="resourceType"/> is null or empty.</exception>
-    public static AuditLog CreateSampleAuditLog(this IntegrationTests tests, string resourceId, string resourceType, AuditAction action)
-    {
-        ArgumentNullException.ThrowIfNull(tests);
-        ArgumentException.ThrowIfNullOrEmpty(resourceId);
-        ArgumentException.ThrowIfNullOrEmpty(resourceType);
+	/// <summary>
+	/// Creates a sample <see cref="AuditLog"/> for testing purposes.
+	/// </summary>
+	/// <param name="tests">The <see cref="IntegrationTests"/> instance.</param>
+	/// <param name="resourceId">The ID of the resource associated with the log.</param>
+	/// <param name="resourceType">The type of the resource.</param>
+	/// <param name="action">The <see cref="AuditAction"/> performed.</param>
+	/// <param name="performedBy">The user or system that performed the action. Defaults to "system".</param>
+	/// <returns>A new <see cref="AuditLog"/> instance.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="tests"/> is null.</exception>
+	/// <exception cref="ArgumentException">Thrown when <paramref name="resourceId"/> or <paramref name="resourceType"/> is null or empty.</exception>
+	public static AuditLog CreateSampleAuditLog(
+		this IntegrationTests tests,
+		string resourceId,
+		string resourceType,
+		AuditAction action,
+		string performedBy = "system")
+	{
+		ArgumentNullException.ThrowIfNull(tests);
+		ArgumentException.ThrowIfNullOrEmpty(resourceId);
+		ArgumentException.ThrowIfNullOrEmpty(resourceType);
 
-        return new AuditLog
-        {
-            Id = Guid.NewGuid().ToString(),
-            ResourceId = resourceId,
-            ResourceType = resourceType,
-            Action = action,
-            IsSuccess = true
-        };
-    }
+		return new AuditLog
+		{
+			Id = Guid.NewGuid().ToString(),
+			ResourceId = resourceId,
+			ResourceType = resourceType,
+			Action = action,
+			PerformedBy = performedBy,
+			IsSuccess = true
+		};
+	}
 
-    /// <summary>
-    /// Creates a sample <see cref="RateLimit"/> for testing purposes.
-    /// </summary>
-    /// <param name="tests">The <see cref="IntegrationTests"/> instance.</param>
-    /// <param name="apiKeyId">The API key ID.</param>
-    /// <param name="requestsPerUnit">The number of requests allowed per unit.</param>
-    /// <returns>A new <see cref="RateLimit"/> instance.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="tests"/> is null.</exception>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="apiKeyId"/> is null or empty.</exception>
-    public static RateLimit CreateSampleRateLimit(this IntegrationTests tests, string apiKeyId, int requestsPerUnit)
-    {
-        ArgumentNullException.ThrowIfNull(tests);
-        ArgumentException.ThrowIfNullOrEmpty(apiKeyId);
+	/// <summary>
+	/// Creates a sample <see cref="RateLimit"/> for testing purposes.
+	/// </summary>
+	/// <param name="tests">The <see cref="IntegrationTests"/> instance.</param>
+	/// <param name="apiKeyId">The API key ID.</param>
+	/// <param name="requestsPerUnit">The number of requests allowed per unit.</param>
+	/// <param name="isEnabled">Whether the rate limit is enabled. Defaults to true.</param>
+	/// <returns>A new <see cref="RateLimit"/> instance.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="tests"/> is null.</exception>
+	/// <exception cref="ArgumentException">Thrown when <paramref name="apiKeyId"/> is null or empty.</exception>
+	public static RateLimit CreateSampleRateLimit(
+		this IntegrationTests tests,
+		string apiKeyId,
+		int requestsPerUnit,
+		bool isEnabled = true)
+	{
+		ArgumentNullException.ThrowIfNull(tests);
+		ArgumentException.ThrowIfNullOrEmpty(apiKeyId);
 
-        return new RateLimit
-        {
-            ApiKeyId = apiKeyId,
-            RequestsPerUnit = requestsPerUnit,
-            Unit = RateLimitUnit.Minute,
-            CurrentRequestCount = 0,
-            LastResetAt = DateTime.UtcNow
-        };
-    }
+		return new RateLimit
+		{
+			Id = Guid.NewGuid().ToString(),
+			ApiKeyId = apiKeyId,
+			RequestsPerUnit = requestsPerUnit,
+			Unit = RateLimitUnit.Minute,
+			IsEnabled = isEnabled,
+			CurrentRequestCount = 0,
+			LastResetAt = DateTime.UtcNow,
+			CreatedAt = DateTime.UtcNow
+		};
+	}
 }
