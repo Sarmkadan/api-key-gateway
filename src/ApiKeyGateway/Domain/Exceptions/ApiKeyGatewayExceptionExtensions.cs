311195fddcddb8af4cb98497bc1d7b7a6031869d
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    refactor(exceptions): small refactor - ApiKeyGatewayExceptionExtensions

--- src/ApiKeyGateway/Domain/Exceptions/ApiKeyGatewayExceptionExtensions.cs
+++ src/ApiKeyGateway/Domain/Exceptions/ApiKeyGatewayExceptionExtensions.cs
@@ -16,10 +16,11 @@ public static class ApiKeyGatewayExceptionExtensions
     /// <summary>
     /// Creates a new <see cref="ApiKeyGatewayException"/> with the same error code and inner exception, but with updated message
     /// </summary>
-    /// <param name="exception">The original exception (cannot be null)</param>
-    /// <param name="message">The new message to use</param>
-    /// <returns>A new exception instance with the updated message</returns>
-    /// <exception cref="ArgumentNullException">Thrown when exception is null</exception>
+    /// <param name="exception">The original exception.</param>
+    /// <param name="message">The new message to use.</param>
+    /// <returns>A new exception instance with the updated message.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="exception"/> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentException"><paramref name="message"/> is <see langword="null"/> or empty</exception>
     public static ApiKeyGatewayException WithMessage(
         this ApiKeyGatewayException exception,
         string message)
@@ -35,10 +36,11 @@ public static class ApiKeyGatewayExceptionExtensions
     /// <summary>
     /// Creates a new <see cref="ApiKeyGatewayException"/> with the same message and inner exception, but with updated error code
     /// </summary>
-    /// <param name="exception">The original exception (cannot be null)</param>
-    /// <param name="errorCode">The new error code to use</param>
-    /// <returns>A new exception instance with the updated error code</returns>
-    /// <exception cref="ArgumentNullException">Thrown when exception is null</exception>
+    /// <param name="exception">The original exception.</param>
+    /// <param name="errorCode">The new error code to use.</param>
+    /// <returns>A new exception instance with the updated error code.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="exception"/> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentException"><paramref name="errorCode"/> is <see langword="null"/> or empty</exception>
     public static ApiKeyGatewayException WithErrorCode(
         this ApiKeyGatewayException exception,
         string errorCode)
@@ -54,10 +56,11 @@ public static class ApiKeyGatewayExceptionExtensions
     /// <summary>
     /// Creates a new <see cref="ApiKeyGatewayException"/> with the same error code and message, but with updated inner exception
     /// </summary>
-    /// <param name="exception">The original exception (cannot be null)</param>
-    /// <param name="innerException">The new inner exception to use</param>
-    /// <returns>A new exception instance with the updated inner exception</returns>
-    /// <exception cref="ArgumentNullException">Thrown when exception is null</exception>
+    /// <param name="exception">The original exception.</param>
+    /// <param name="innerException">The new inner exception to use.</param>
+    /// <returns>A new exception instance with the updated inner exception.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="exception"/> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentNullException"><paramref name="innerException"/> is <see langword="null"/></exception>
     public static ApiKeyGatewayException WithInnerException(
         this ApiKeyGatewayException exception,
         Exception innerException)
@@ -71,13 +74,13 @@ public static class ApiKeyGatewayExceptionExtensions
     }
 
     /// <summary>
-    /// Determines whether the exception has a specific error code
+    /// Determines whether the exception has a specific error code.
     /// </summary>
-    /// <param name="exception">The exception to check (cannot be null)</param>
-    /// <param name="errorCode">The error code to match against</param>
-    /// <returns>True if the exception has the specified error code; otherwise false</returns>
-    /// <exception cref="ArgumentNullException">Thrown when exception is null</exception>
-    /// <exception cref="ArgumentNullException">Thrown when errorCode is null</exception>
+    /// <param name="exception">The exception to check.</param>
+    /// <param name="errorCode">The error code to match against.</param>
+    /// <returns><see langword="true"/> if the exception has the specified error code; otherwise <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="exception"/> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentException"><paramref name="errorCode"/> is <see langword="null"/> or empty</exception>
     public static bool HasErrorCode(
         this ApiKeyGatewayException exception,
         string errorCode)
