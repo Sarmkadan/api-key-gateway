a851dae7fb3807a7d8e9ef7ce3875ca0ffd9d0f0
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    Polish ApiResponseBuilderValidation
    also touched a couple of call sites.

--- src/ApiKeyGateway/Utilities/ApiResponseBuilderValidation.cs
+++ src/ApiKeyGateway/Utilities/ApiResponseBuilderValidation.cs
@@ -15,74 +15,53 @@ public static class ApiResponseBuilderValidation
     /// <param name="value">The API response builder to validate.</param>
     /// <returns>A list of validation problems (empty if valid).</returns>
     /// <exception cref="ArgumentNullException">Thrown if <paramref name="value" /> is null.</exception>
-    public static IReadOnlyList<string> Validate<T>(this ApiResponseBuilder<T>? value)
+    public static IReadOnlyList<string> Validate<T>(this ApiResponseBuilder<T> value)
     {
         ArgumentNullException.ThrowIfNull(value);
 
         var problems = new List<string>();
 
-        // Validate Success property by attempting to set it to null state
-        try
-        {
-            value.Success(null);
-        }
-        catch
-        {
-            problems.Add("Success() method must be called before building.");
-        }
+        // Validate that either Success or Error was called by checking the message field
+        // Success sets message to "Success" or provided message, Error sets message to error message
+        // If neither was called, message would remain null
+        var messageField = value.GetType().GetField("_message", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+        var messageValue = messageField?.GetValue(value) as string;
 
-        // Validate Error property by attempting to set it to null state
-        try
-        {
-            value.Error(200, string.Empty);
-        }
-        catch
-        {
-            problems.Add("Error() method must be called before building.");
-        }
-
-        // Validate Data property by attempting to set it to null for reference types
-        try
-        {
-            var builder = new ApiResponseBuilder<object?>();
-            _ = builder.WithData(null);
-        }
-        catch
-        {
-            problems.Add("WithData() must be called with valid data.");
-        }
-
-        // Validate WithMetadata by attempting to add empty metadata
-        try
-        {
-            var builder = new ApiResponseBuilder<object>();
-            _ = builder.WithMetadata(string.Empty, null!);
-        }
-        catch
-        {
-            problems.Add("WithMetadata() requires a non-empty key.");
-        }
-
-        // Validate AddError by attempting to add empty error
-        try
-        {
-            var builder = new ApiResponseBuilder<object>();
-            _ = builder.AddError(string.Empty);
-        }
-        catch
-        {
-            problems.Add("AddError() requires a non-empty error message.");
-        }
-
-        // Validate that at least one of Success or Error was called
-        try
-        {
-            var builder = new ApiResponseBuilder<object>();
-            _ = builder.Build();
-        }
-        catch
+        if (string.IsNullOrEmpty(messageValue))
         {
             problems.Add("Either Success() or Error() must be called before building.");
+        }
+
+        // Validate metadata keys are not empty
+        var metadataField = value.GetType().GetField("_metadata", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+        var metadataValue = metadataField?.GetValue(value) as Dictionary<string, object>;
+
+        if (metadataValue != null)
+        {
+            foreach (var key in metadataValue.Keys)
+            {
+                if (string.IsNullOrEmpty(key))
+                {
+                    problems.Add("WithMetadata() requires a non-empty key.");
+                    break;
+                }
+            }
+        }
+
+        // Validate error messages are not empty
+        var errorsField = value.GetType().GetField("_errors", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+        var errorsValue = errorsField?.GetValue(value) as List<string>;
+
+        if (errorsValue != null)
+        {
+            foreach (var error in errorsValue)
+            {
+                if (string.IsNullOrEmpty(error))
+                {
+                    problems.Add("AddError() requires a non-empty error message.");
+                    break;
+                }
+            }
         }
 
         return problems.AsReadOnly();
@@ -95,9 +74,9 @@ public static class ApiResponseBuilderValidation
     /// <param name="value">The API response builder to check.</param>
     /// <returns>True if the builder is valid; otherwise, false.</returns>
     /// <exception cref="ArgumentNullException">Thrown if <paramref name="value" /> is null.</exception>
-    public static bool IsValid<T>(this ApiResponseBuilder<T>? value)
+    public static bool IsValid<T>(this ApiResponseBuilder<T> value)
     {
-        return value?.Validate().Count == 0;
+        return value is not null && value.Validate().Count == 0;
     }
 
     /// <summary>
@@ -107,7 +86,7 @@ public static class ApiResponseBuilderValidation
     /// <param name="value">The API response builder to validate.</param>
     /// <exception cref="ArgumentNullException">Thrown if <paramref name="value" /> is null.</exception>
     /// <exception cref="ArgumentException">Thrown if the builder contains validation problems.</exception>
-    public static void EnsureValid<T>(this ApiResponseBuilder<T>? value)
+    public static void EnsureValid<T>(this ApiResponseBuilder<T> value)
     {
         ArgumentNullException.ThrowIfNull(value);
 
