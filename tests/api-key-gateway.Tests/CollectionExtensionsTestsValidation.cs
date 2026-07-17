fe6469a3d61f387b7907386037fd0cbd80b7c069
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    dedupe some logic in CollectionExtensionsTestsValidation

--- tests/api-key-gateway.Tests/CollectionExtensionsTestsValidation.cs
+++ tests/api-key-gateway.Tests/CollectionExtensionsTestsValidation.cs
@@ -1,49 +1,41 @@
 using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
 
 namespace ApiKeyGateway.Tests;
 
+/// <summary>
+/// Provides validation extension methods for <see cref="CollectionExtensionsTests"/> instances.
+/// </summary>
 public static class CollectionExtensionsTestsValidation
 {
     /// <summary>
     /// Validates the <see cref="CollectionExtensionsTests"/> instance.
     /// </summary>
     /// <param name="value">The instance to validate.</param>
-    /// <returns>A list of human-readable problems.</returns>
+    /// <returns>A list of human-readable problems; empty if valid.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
     public static IReadOnlyList<string> Validate(this CollectionExtensionsTests value)
     {
         ArgumentNullException.ThrowIfNull(value);
 
-        var problems = new List<string>();
-
-        // No specific properties to validate, as CollectionExtensionsTests seems to be a test class with methods only.
-
-        return problems.AsReadOnly();
+        return Array.Empty<string>();
     }
 
     /// <summary>
     /// Checks if the <see cref="CollectionExtensionsTests"/> instance is valid.
     /// </summary>
     /// <param name="value">The instance to validate.</param>
-    /// <returns>true if the instance is valid; otherwise, false.</returns>
-    public static bool IsValid(this CollectionExtensionsTests value)
-    {
-        return Validate(value).Count == 0;
-    }
+    /// <returns><see langword="true"/> if the instance is valid; otherwise, <see langword="false"/>.</returns>
+    public static bool IsValid(this CollectionExtensionsTests value) =>
+        value is not null;
 
     /// <summary>
     /// Ensures that the <see cref="CollectionExtensionsTests"/> instance is valid.
     /// </summary>
     /// <param name="value">The instance to validate.</param>
-    /// <exception cref="ArgumentException">Thrown if the instance is not valid.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
     public static void EnsureValid(this CollectionExtensionsTests value)
     {
-        var problems = Validate(value);
-        if (problems.Count > 0)
-        {
-            throw new ArgumentException($"Invalid CollectionExtensionsTests: {string.Join(", ", problems)}", nameof(value));
-        }
+        ArgumentNullException.ThrowIfNull(value);
     }
 }
