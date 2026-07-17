3655988c75018fd947874553286a4ab64f25206c
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 19:31:38 +0300

    dedupe some logic in ValidationExceptionExtensions

--- src/ApiKeyGateway/Domain/Exceptions/ValidationExceptionExtensions.cs
+++ src/ApiKeyGateway/Domain/Exceptions/ValidationExceptionExtensions.cs
@@ -16,34 +16,35 @@ public static class ValidationExceptionExtensions
         ArgumentNullException.ThrowIfNull(exception);
 
         var message = exception.Message;
-        if (exception.ParameterName is { } parameterName)
+
+        if (exception.ParameterName is { Length: > 0 } parameterName)
         {
             message += $" (Parameter: {parameterName})";
         }
 
-        if (exception.AttemptedValue is { } attemptedValue)
+        if (exception.AttemptedValue is not null)
         {
-            message += $" Attempted value: {attemptedValue}";
+            message += $" Attempted value: {exception.AttemptedValue}";
         }
 
-        if (exception.ValidationErrors is { } validationErrors)
+        if (exception.ValidationErrors?.Any() == true)
         {
-            message += Environment.NewLine + string.Join(Environment.NewLine, validationErrors);
+            message += Environment.NewLine + string.Join(Environment.NewLine, exception.ValidationErrors);
         }
 
         return message;
     }
 
     /// <summary>
-    /// Determines if a <see cref="ValidationException"/> has any validation errors.
+    /// Determines if a <see cref="ValidationException"/> contains any validation errors.
     /// </summary>
-    /// <param name="exception">The <see cref="ValidationException"/> to check.</param>
-    /// <returns><c>true</c> if the exception has any validation errors; otherwise, <c>false</c>.</returns>
-    /// <exception cref="ArgumentNullException">Thrown if <paramref name="exception"/> is null.</exception>
+    /// <param name="exception">The <see cref="ValidationException"/> to check for validation errors.</param>
+    /// <returns><c>true</c> if the exception contains one or more validation errors; otherwise, <c>false</c>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="exception"/> is <see langword="null"/>.</exception>
     public static bool HasValidationErrors(this ValidationException exception)
     {
         ArgumentNullException.ThrowIfNull(exception);
 
-        return exception.ValidationErrors?.Any() ?? false;
+        return exception.ValidationErrors?.Any() == true;
     }
 }
