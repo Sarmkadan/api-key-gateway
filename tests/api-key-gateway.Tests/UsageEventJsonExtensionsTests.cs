41354602073f23901b8813cd9791bbfa6489e27a
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-18 08:57:33 +0000

    chore: apply workspace updates

---
+++ tests/api-key-gateway.Tests/UsageEventJsonExtensionsTests.cs
@@ -0,0 +1,56 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ApiKeyGateway.Events;
+using Moq;
+
+namespace ApiKeyGateway.Tests
+{
+    public class UsageEventJsonExtensionsTests
+    {
+        [Fact]
+        public async Task Test_ToJson_Happy_Path()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task Test_FromJson_Happy_Path()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task Test_TryFromJson_Happy_Path()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task Test_ToJson_Null_Input()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task Test_FromJson_Empty_Collection()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task Test_TryFromJson_Boundary_Values()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task Test_ToJson_Error_Path()
+        {
+            // Test implementation
+        }
+    }
+}
