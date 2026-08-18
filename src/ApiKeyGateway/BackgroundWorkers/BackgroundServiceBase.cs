a5956c0dd07a90628250d54370f04ee6e79e60ad
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-18 03:27:50 +0000

    fix: pass stoppingToken to Task.Delay

--- src/ApiKeyGateway/BackgroundWorkers/BackgroundServiceBase.cs
+++ src/ApiKeyGateway/BackgroundWorkers/BackgroundServiceBase.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Threading;
 
 namespace ApiKeyGateway.BackgroundWorkers;
 
